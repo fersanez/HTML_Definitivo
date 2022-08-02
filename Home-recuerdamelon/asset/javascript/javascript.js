82fa0674583180605fddba4f905f4cef7a8d464b
fersanez <105713484+fersanez@users.noreply.github.com>
2022-08-02 19:55:09 +0200

    Actualizado JS PopUp-NewTask

--- Home-recuerdamelon/asset/javascript/javascript.js
+++ Home-recuerdamelon/asset/javascript/javascript.js
@@ -34,3 +34,73 @@ function filterFunction() {
     }
   }
 }
+
+
+
+                // -------- POPUP CREAR TASK ---------------// 
+ 
+function openNewTask(){
+  document.getElementById("formNewTask").style.display ="block";
+}
+
+function clearNewTask(){
+  document.getElementById("tittle-task").value=""
+  document.getElementById("description-task").value=""
+  document.getElementById("description-task").value=""
+  document.getElementById("dateTask-ini").value=""
+  document.getElementById("dateTask-fin").value=""
+  document.getElementById("catTask").value=""
+  document.getElementById("shareTask").value=""
+  document.getElementById("cont-span").textContent="0"
+}
+
+function cancelNewTask(){
+  document.getElementById("formNewTask").style.display ="none";
+}
+
+function acceptNewTask(){
+  document.getElementById("formNewTask").style.display ="none";
+}
+
+function myShared() {
+  document.getElementById("myContacts").classList.toggle("show");
+}
+
+
+
+// -------- CONTADOR CHAR TITTLES CREAR TASK
+// -------- AUX
+function contchar(){
+  var inputsCont = "input[maxlength], textarea[maxlength]";
+  $(document).on('keyup', "[maxlength]", function (e) {
+    var este = $(this),
+      maxlength = este.attr('maxlength'),
+      maxlengthint = parseInt(maxlength),
+      textoActual = este.val(),
+      currentCharacters = este.val().length;
+      remainingCharacters = currentCharacters,
+      espan = este.prev('label').find('span');      
+      // Detectamos si es IE9 y si hemos llegado al final, convertir el -1 en 0 - bug ie9 porq. no coge directamente el atributo 'maxlength' de HTML5
+      if (document.addEventListener && !window.requestAnimationFrame) {
+        if (remainingCharacters <= -1) {
+          remainingCharacters = 0;            
+        }
+      }
+      espan.html(remainingCharacters);
+      if (!!maxlength) {
+        var texto = este.val(); 
+        if (texto.length >= maxlength) {
+          // este.removeClass().addClass("borderojo");
+          este.val(text.substring(0, maxlength));
+          e.preventDefault();
+        }
+        else if (texto.length < maxlength) {
+          este.removeClass().addClass("bordegris");
+        } 
+      } 
+});
+}
+
+
+
+                // -------- POPUP CREAR TASK ---------------//
